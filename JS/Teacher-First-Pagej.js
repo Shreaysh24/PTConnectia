de9a60332ab3cd406b42d7aaae61b1c82aef7a8f
Shreaysh24 <shreayshrc@gmail.com>
2023-11-04 09:27:21 +0530

    added profile & abot section

---
+++ JS/Teacher-First-Pagej.js
@@ -0,0 +1,7 @@
+$(".tfp-nav .tfp-img:last-child").on("click",function(){
+    $(".in").toggleClass('active');
+});
+
+$(".tfp-nav .tfp-img:first-child").on("click",function(){
+    $(".out").toggleClass("deActive");
+});
