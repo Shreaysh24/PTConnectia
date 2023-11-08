61d687d3a77713a01ed2b39a68d3f7c0a1274d2d
Shreaysh24 <shreayshrc@gmail.com>
2023-11-09 00:08:48 +0530

    Aplied Show & Hide Pass Logic

--- JS/Teacher-First-Pagej.js
+++ JS/Teacher-First-Pagej.js
@@ -1,3 +1,19 @@
+// password logic
+
+
+$("form .inputContainer img").click(function(){
+  var p = document.getElementById("pwd");
+  if(p.type == "password"){
+    p.type = "text";
+  }
+  else{
+    p.type = "password";
+  }
+});
+
+
+// teacher profile
+
 $(".tfp-nav .tfp-img:last-child").on("click",function(){
     $(".in").toggleClass('active');
 });
@@ -5,3 +21,5 @@ $(".tfp-nav .tfp-img:last-child").on("click",function(){
 $(".tfp-nav .tfp-img:first-child").on("click",function(){
     $(".out").toggleClass("deActive");
 });
+
+
